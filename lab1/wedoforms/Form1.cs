8fe0ec77a833f6e292a0a295636e73777b5e8f62
Messi <wisenorwegian@gmail.com>
2025-02-18 10:25:40 +0500

    bruh

--- lab1/wedoforms/Form1.cs
+++ lab1/wedoforms/Form1.cs
@@ -45,12 +45,10 @@ namespace wedoforms
                 if (parsed.Length > 2)
                 {
                     while(parsed.Last() == '0' && parsed.Length>2)
-                    {
-                        parsed = parsed.Remove(parsed.Length - 1);
-                    }
+                    { parsed = parsed.Remove(parsed.Length - 1); }
                 }
                 if (parsed.Length > 0) { if (parsed.Last() == '.') { parsed = parsed.Remove(parsed.Length - 1); } }
-                listBox1.Items.Add(parsed);
+                if (parsed != "-") { listBox1.Items.Add(parsed); }
             }
             else { MessageBox.Show("there's nothing"); }
         }
@@ -69,7 +67,19 @@ namespace wedoforms
 
         private void toolStripMenuItemdIV_Click(object sender, EventArgs e)
         {
-
+            double multi = 1;
+            double count = 0;
+            string result = "";
+            foreach (string parsed in listBox1.Items.OfType<string>().ToList())
+            {
+                if (double.Parse(parsed) > 0)
+                {
+                    multi *= double.Parse(parsed) - Int32.Parse(parsed.Split('.')[0]);
+                    count++;
+                }
+            }
+            if (count > 0) { result = Math.Pow(multi, 1 / count).ToString(); }
+            if (result != "") { listBox2.Items.Add(result); }
         }
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {this.Text = "praise the "+comboBox1.SelectedItem.ToString()+"!";}
@@ -180,7 +190,27 @@ namespace wedoforms
         }
         private void ToolStripMenuItemIV_Click(object sender, EventArgs e)
         {
-
+            List<int> rating = new List<int>();
+            int maxcount = 0;
+            int count = 0;
+            string result = "";
+            foreach (string parsed in listBox3.Items.OfType<string>().ToList()) {
+                bool space = false;
+                char s = parsed.First();
+                foreach (char g in parsed){
+                    if (g == ' ') { space = true; }
+                    else if (space) { if (g == s) { count++; space = false; } }
+                }
+                if (count > maxcount) { maxcount = count; }
+                rating.Add(count);
+                count = 0;
+            }
+            for (int i = 0; i < rating.Count; i++)
+            {
+                if (rating[i] == maxcount)
+                { result += listBox3.Items.OfType<string>().ToList()[i] + "\n"; }
+            }
+            MessageBox.Show(result);
         }
 
         private void menuStrip1_ItemClicked_1(object sender, ToolStripItemClickedEventArgs e)
